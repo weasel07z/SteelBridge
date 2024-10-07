c1d9576a3b4f5ab84ab0132ffe894e177a9d3438
weasel07z <moronsbob@gmail.com>
2024-10-07 13:27:51 -0500

    kdfjsdkfj

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { getDatabase, ref, set, onValue, get, child, query, orderByChild } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, set, onValue, query, orderByChild } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDVLdga-K3mays-RQmEnIbUzUSY8JxH5x0",
   authDomain: "steel-bridge.firebaseapp.com",
@@ -20,48 +17,78 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const topScores = query(ref(db, 'Scores'), orderByChild('points'));
+const northScores = query(ref(db, 'North'), orderByChild('cost'));
+const southScores = query(ref(db, 'South'), orderByChild('cost'));
 
-function addScore(name, points){
-    if(name == "" || points == "") return;
-    set(ref(db, 'Scores/' + name), {
-        username: name,
-        points: points
-    });
+function addScore(name, cost, direction) {
+    if (name == "" || cost == "") return;
+    if (direction == "South") {
+        set(ref(db, 'South/' + name), {
+            username: name,
+            cost: cost
+        });
+    } else if (direction == "North") {
+        set(ref(db, 'North/' + name), {
+            username: name,
+            cost: cost
+        });
+    }
 }
+function resetDatabase() {
+    set(ref(db, 'North'), {});
+    set(ref(db, 'South'), {});
+    printLeaderboard();
+}
+
 document.getElementById("uploadButton").addEventListener("click", () => {
-    // Dynamically load and execute the script.js file when the button is clicked
-    import('./script.js').then((module) => {
-        module.readCSV(); // Assuming script.js exports a function called runScript
-    }).catch(error => {
-        console.error('Error loading script.js:', error);
-    });
+    if(window.isResetConfirmed == true) {
+        resetDatabase();
+    } else if(document.getElementById("name").value == "RESET") {
+        return;
+    }else {
+        setTimeout(function() {
+            var name = document.getElementById("name").value;
+            var direction = document.getElementById("bridgeSide").value;
+            var cost = parseInt(document.getElementById("cost").innerHTML);
+            
+            addScore(name, cost, direction);
+        }, 200);
+    }
+    printLeaderboard();
 });
-// document.getElementById("uploadButton").addEventListener("click", () => {
-//     // var name = document.getElementById("name").value;
-//     // var points = parseInt(document.getElementById("score").value);
-//     // console.log(name, points);
-//     // addScore(name,points);
-//     // printLeaderboard();
-
-// });
-
-console.log(app);
-
-export function printLeaderboard(){
-    onValue(topScores, function(snapshot) {
-        var leaderboardList = document.getElementById('leaderboardList');
-        leaderboardList.innerHTML = '';
+export function printLeaderboard() {
+    // North Scores
+    onValue(northScores, function(snapshot) {
+        var northLeaderboard = document.getElementById('northLeaderboardList');
+        northLeaderboard.innerHTML = '';
 
         snapshot.forEach(function(childSnapshot) {
             var data = childSnapshot.val();
             var playerName = data.username;
-            var playerPoints = data.points;
-            playerName = playerName.substring(0,30);
+            var playerPoints = data.cost;
+            playerName = playerName.substring(0, 30);
             var li = document.createElement('li');
-            li.innerHTML = '<span id="words">' + playerName + '</span><span>' + playerPoints + ' points</span>';
-            leaderboardList.prepend(li);
+            li.innerHTML = '<span>' + playerName + '</span><span>$' + playerPoints + '</span>';
+            northLeaderboard.append(li);
+        });
+    });
+
+    // South Scores
+    onValue(southScores, function(snapshot) {
+        var southLeaderboard = document.getElementById('southLeaderboardList');
+        southLeaderboard.innerHTML = '';
+
+        snapshot.forEach(function(childSnapshot) {
+            var data = childSnapshot.val();
+            var playerName = data.username;
+            var playerPoints = data.cost;
+            playerName = playerName.substring(0, 30);
+            var li = document.createElement('li');
+            li.innerHTML = '<span>' + playerName + '</span><span>$' + playerPoints + '</span>';
+            southLeaderboard.append(li);
         });
     });
 }
+
 printLeaderboard();
+
